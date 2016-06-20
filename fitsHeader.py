6185a981ee7a1f847e7560fa76a96bfa3704c832
DanielAndreasen <daniel.andreasen@astro.up.pt>
2016-06-20 20:23:28 +0100

    Show info for many files

--- fitsHeader.py
+++ fitsHeader.py
@@ -10,22 +10,25 @@ import argparse
 
 def _parser():
     parser = argparse.ArgumentParser(description='View the header of a fits file')
-    parser.add_argument('input', help='File name of fits file')
+    parser.add_argument('input', help='File name of fits file', nargs='+')
     parser.add_argument('-key', help='Look up a given key (case insensitive)', default=None)
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = _parser()
-    h = fits.getheader(args.input)
-    h.keys = map(str.lower, h.keys())
 
     if args.key:
         args.key = args.key.lower()
-        try:
-            print h[args.key]
-        except KeyError:
-            raise KeyError('Key was not found')
+        for fname in args.input:
+            h = fits.getheader(fname)
+            h.keys = map(str.lower, h.keys())
+            try:
+                print '%s:  %s' % (fname, h[args.key])
+            except KeyError:
+                print 'Key was not found in: %s' % fname
+                pass
     else:
+        h = fits.getheader(args.input[0])
         string = '\n'.join("{!s} : {!r}".format(key, val) for (key, val) in h.items())
         pager(string)
